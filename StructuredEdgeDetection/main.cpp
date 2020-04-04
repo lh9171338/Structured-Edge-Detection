a393093f795becebff99e52e1e1679330f71bcd1
lh9171338 <2909171338@qq.com>
2020-04-04 22:50:29 +0800

    2020-04-04

---
+++ StructuredEdgeDetection/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/ximgproc.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace cv::ximgproc;
+
+
+int main(int argc, char** argv)
+{
+	/******************* 参数 *******************/
+	string srcImageFilename = "src.jpg";
+	string dstImageFilename = "dst.jpg";
+	String modelFilename = "model.yml";
+	bool showFlag = true;
+	bool saveFlag = false;
+	if (argc == 3)
+	{
+		srcImageFilename = argv[1];
+		dstImageFilename = argv[2];
+		showFlag = false;
+		saveFlag = true;
+	}
+
+	/******************* 读取图像 *******************/
+	Mat srcImage = imread(srcImageFilename);
+	if (srcImage.empty())
+		cerr << "Read image Failed!" << endl;
+
+	/******************* 边缘检测 *******************/
+	Mat image, edge, dstImage;
+	Mat orientation;
+	Mat finalEdge;
+	srcImage.convertTo(image, DataType<float>::type, 1 / 255.0);
+	Ptr<StructuredEdgeDetection> pDollar = createStructuredEdgeDetection(modelFilename);
+	pDollar->detectEdges(image, edge);
+	pDollar->computeOrientation(edge, orientation);
+	pDollar->edgesNms(edge.clone(), orientation, edge, 2, 0, 1, true);
+	edge.convertTo(dstImage, DataType<uchar>::type, 255.0);
+	threshold(dstImage.clone(), dstImage, 0, 255, THRESH_OTSU);
+
+	/******************* 保存结果 *******************/
+	if (showFlag) {
+		namedWindow("srcImage", 0);
+		namedWindow("dstImage", 0);
+		imshow("srcImage", srcImage);
+		imshow("dstImage", dstImage);
+		waitKey();
+	}
+	if (saveFlag)
+		imwrite(dstImageFilename, dstImage);
+
+	return 0;
+}
